11c2e92a2001971a7f8e3f42414603b5503cd1fe
DanielGao97 <76081240+DanielGao97@users.noreply.github.com>
2021-07-03 21:06:59 +0800

    Add files via upload

--- jd_qjd.py
+++ jd_qjd.py
@@ -13,7 +13,7 @@ update: 2021.7.3 15:41
 
 #ck 优先读取【JDCookies.txt】 文件内的ck  再到 ENV的 变量 JD_COOKIE='ck1&ck2' 最后才到脚本内 cookies=ck
 cookies = ''
-qjd_zlzh = ['Your JD_User', '买买买', '东哥']
+qjd_zlzh = ['法外狂徒joker', 'jd_ESFgYELRIxPu']
 
 # 建议调整一下的参数
 # UA 可自定义你的，注意格式
